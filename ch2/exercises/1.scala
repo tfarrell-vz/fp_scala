b39b3f8d0aa4801eca5f043714722b09a028dae0
tfarrell-vz <farrell@abiresearch.com>
2015-05-12 17:39:30 -0400

    Used tailrec annotation to ensure tail call elimination was successful.

--- ch2/exercises/1.scala
+++ ch2/exercises/1.scala
@@ -1,12 +1,15 @@
 // Recursive function to get the nth Fibonacci number
 // No loops allowed: Think functionally.
 
-def fib(n: Int): Int = {
-  def loop(prev: Int, next: Int, count: Int): Int = {
-    if (count == 0) prev
-    else loop(next, prev+next, count-1)
+object MainModule {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(prev: Int, next: Int, count: Int): Int = {
+      if (count == 0) prev
+      else loop(next, prev+next, count-1)
+    }
+    loop(0, 1, n)
   }
-  loop(0, 1, n)
 }
 
 
