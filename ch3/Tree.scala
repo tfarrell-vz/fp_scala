2a50cefeeb1c4f959885a8110644c27db8d1a88a
tfarrell-vz <farrell@abiresearch.com>
2015-10-02 15:06:10 -0400

    size function counts number of nodes in a tree

--- ch3/Tree.scala
+++ ch3/Tree.scala
@@ -1,3 +1,10 @@
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(a) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
+}
