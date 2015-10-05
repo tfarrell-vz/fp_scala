25506469a48951b9ac1fc897fd950017eb14b6d4
tfarrell-vz <farrell@abiresearch.com>
2015-10-05 10:23:37 -0400

    Practicing with an assignment. lastDigit and dropLastDigit implemented

---
+++ ch3/CreditCard.scala
@@ -0,0 +1,15 @@
+/**
+ * www.seas.upenn.edu/~cis194/hw/01-intro.pdf
+ */
+
+/**
+ * Exercise 1
+ */
+
+def lastDigit(num: Int): Int = num % 10
+def dropLastDigit(num: Int): Int = num / 10
+
+/**
+  * Exercise 2
+  */
+
