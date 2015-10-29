99299b03858f853fbd23394aff12fe1c70d76fbc
tfarrell-vz <farrell@abiresearch.com>
2015-10-29 12:43:52 -0400

    Example of Option, and functions to implement

--- ch4/Option.scala
+++ ch4/Option.scala
@@ -1,4 +1,18 @@
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  /* TODO: Implement these functions on Option
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
+  */
+}
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
+object options {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+}
