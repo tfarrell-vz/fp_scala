6e1980acb6546e49d085be1b4d7fa9f22ffb2faf
tfarrell-vz <farrell@abiresearch.com>
2015-11-01 20:16:02 -0500

    flatMap

--- ch4/Option.scala
+++ ch4/Option.scala
@@ -19,11 +19,20 @@ object Option {
     case None => None
     case Some(a) => Some(f(a))
   }
-  
+
+  def flatMap[A, B](opt: Option[A], f: A => Option[B]): Option[B] = opt match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
 }
 
 object options {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
+  def div(x: Int, y: Int): Option[Int] =
+    if (y == 0) None
+    else Some(x/y)
 }
